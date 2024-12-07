5e44bcd409119acdf91f67d750335d7bf020e0f1
ZOHAIB7689 <bzohaib754@gmail.com>
2024-12-08 02:21:31 +0500

    initial commit

---
+++ components/NavbarComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className=' w-full  flex flex-col'>
+      <div className=" flex md:hidden justify-between max-w-7xl bg-[#272343]">
+
+      </div>
+      <div className=""></div>
+      
+    </div>
+  )
+}
